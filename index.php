af3b9d4391972ab034877f22e9f928fd8f4d5429
Marcus Burnette <marcus@flitehaus.com>
2014-02-01 02:34:41 -0500

    cleaned up demo index page styles

--- index.php
+++ index.php
@@ -3,27 +3,67 @@
 <head>
 
 <meta charset="utf-8" />
-<title>Formatter.js Credit Card Plugin</title>
+<title>Formatter.js Plugin Credit Card Extension</title>
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="jquery.formatter.min.js" type="text/javascript"></script>
 <script src="jquery.formattercc.js" type="text/javascript"></script>
 
+<style type="text/css">
+body {
+background: #f7f7f7;
+font-family: sans-serif;
+text-align: center;
+padding: 100px 0 0 0;
+}
+
+p {
+margin-bottom: 2em;
+}
+
+input, select {
+padding: 0.5em 1em;
+border: 0;
+background: #fff;
+border: 1px solid #ddd;
+border-radius: 4px;
+font-size: 1em;
+}
+
+#ccnum {
+margin-right: 1em;
+width: 14em;
+}
+
+#ccvnum {
+width: 4em;
+}
+
+a {
+color: #1998c4;
+}
+</style>
+
 </head>
 
 <body>
 
-<div>
-    Credit Card Type: 
+<h1>Formatter.js Plugin Credit Card Extension</h1>
+
+<p>This plugin extends the <a href="http://firstopinion.github.io/formatter.js/">formatter.js by firstopinion</a> jQuery plugin to easily handle credit card patterns.</p>
+
+<div class="cardForm">
+    <p>Credit Card Type: 
     <select id="cctype">
         <option value="visa">Visa</option>
         <option value="mc">Mastercard</option>
         <option value="discover">Discover</option>
         <option value="amex">American Express</option>
     </select>
-    <br/>
-    Credit Card #: 
-    <input id="ccnum" class="ccnum" type="text" /> CCV: <input id="ccvnum" class="ccvnum" type="text" /> 
+    </p>
+
+    <p>Credit Card #: 
+    <input id="ccnum" class="ccnum" type="text" /> CCV: <input id="ccvnum" class="ccvnum" type="text" /></p>
 </div>
 
 <script type="text/javascript">
